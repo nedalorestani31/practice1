da7445709c04298ab2062f18085cf12dc82dd73d
nedalorestani31 <nedalorestani31@gmail.com>
2019-01-12 23:51:44 +0330

    problem in javascript code

---
+++ script.js
@@ -0,0 +1,10 @@
+let allH1s = document.getElementsByTagName('h1');
+allH1s[0].innerText= 'NEDA LORESTANI';
+
+
+let allneda = document.getElementsByClassName("allneda");
+allneda[0].innerHTML= "practic";
+
+
+let alllorestanis = document.getElementById("alllorestani");
+alllorestanis[0] . innerText="NEDA";
