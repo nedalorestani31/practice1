88fe6127467287b945e4759b72d512fe86f9817e
Majid <majid88rahimi@gmail.com>
2019-01-13 18:16:40 +0200

    Fixed the issue with script

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let allH1s = document.getElementsByTagName('h1');
-allH1s[0].innerText= 'NEDA LORESTANI';
+allH1s[0].innerText = 'NEDA LORESTANI';
 
 
-let allneda = document.getElementsByClassName("allneda");
-allneda[0].innerHTML= "practic";
+let allneda = document.getElementsByClassName("neda");
+// console.log(allneda);
+
+allneda[0].innerHTML = "practic";
 
 
-let alllorestanis = document.getElementById("alllorestani");
-alllorestanis[0] . innerText="NEDA";
+let alllorestanis = document.getElementById("lorestani");
+alllorestanis.innerText = "NEDA";
